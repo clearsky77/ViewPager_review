660ecaba93765be698297a0c07e66618aead98db
clearsky77 <clearsky3264@gmail.com>
2021-10-01 00:08:39 +0900

    프래그먼트 3개 중 3개 완성. 2.이름 / 3. 나이 프래그먼트까지 완성

---
+++ app/src/main/java/com/clearsky77/viewpager_review/fragments/NameFragment.kt
@@ -0,0 +1,23 @@
+package com.clearsky77.viewpager_review.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.clearsky77.viewpager_review.R
+
+class NameFragment : Fragment(){
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_name, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+//        이벤트 처리
+    }
+}
