5415e9145937f3f8dfe268114e8efc99e170ebcc
clearsky77 <clearsky3264@gmail.com>
2021-10-02 14:53:01 +0900

    프래그먼트 페이징 담당하는 어댑터 작업중 - 함수 2개 작성   - 몇 장으로 구성되었나   - 위치에 어떤 Fragment가 표현되는가

--- app/src/main/java/com/clearsky77/viewpager_review/adapters/ManinViewPagerAdapter.kt
+++ app/src/main/java/com/clearsky77/viewpager_review/adapters/ManinViewPagerAdapter.kt
@@ -3,14 +3,24 @@ package com.clearsky77.viewpager_review.adapters
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentPagerAdapter
+import com.clearsky77.viewpager_review.fragments.AgeFragment
+import com.clearsky77.viewpager_review.fragments.GreetingFragment
+import com.clearsky77.viewpager_review.fragments.NameFragment
 
 class ManinViewPagerAdapter(fm : FragmentManager) : FragmentPagerAdapter(fm) {
+    // 몇 장의 뷰를 만들것인가?
     override fun getCount(): Int {
-
+        return 3
     }
-
+    // 순서(번호)에 따른 프래그먼트 반환
     override fun getItem(position: Int): Fragment {
-
+        if(position == 0){
+            return GreetingFragment()
+        }else if(position == 1){
+            return NameFragment()
+        }else{
+            return AgeFragment()
+        }
     }
 
 }
