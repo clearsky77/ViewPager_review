c90c01b5c64e084e3c74bb3de6ed681fa3f86756
clearsky77 <clearsky3264@gmail.com>
2021-10-02 15:24:03 +0900

    탭레이아웃에 보여지는 페이지별 탭 제목 ViewPagerAdapter에서 작성함을 주의

--- app/src/main/java/com/clearsky77/viewpager_review/adapters/MainViewPagerAdapter.kt
+++ app/src/main/java/com/clearsky77/viewpager_review/adapters/MainViewPagerAdapter.kt
@@ -7,20 +7,32 @@ import com.clearsky77.viewpager_review.fragments.AgeFragment
 import com.clearsky77.viewpager_review.fragments.GreetingFragment
 import com.clearsky77.viewpager_review.fragments.NameFragment
 
-class MainViewPagerAdapter(fm : FragmentManager) : FragmentPagerAdapter(fm) {
+class MainViewPagerAdapter(fm: FragmentManager) : FragmentPagerAdapter(fm) {
+
+    // 탭레이아웃 페이지별 탭 제목
+    override fun getPageTitle(position: Int): CharSequence? {
+        if (position == 0) {
+            return "인사말"
+        } else if (position == 1) {
+            return "이름"
+        } else {
+            return "나이"
+        }
+    }
+
     // 몇 장의 뷰를 만들것인가?
     override fun getCount(): Int {
         return 3
     }
+
     // 순서(번호)에 따른 프래그먼트 반환
     override fun getItem(position: Int): Fragment {
-        if(position == 0){
+        if (position == 0) {
             return GreetingFragment()
-        }else if(position == 1){
+        } else if (position == 1) {
             return NameFragment()
-        }else{
+        } else {
             return AgeFragment()
         }
     }
-
 }
