f64eefd7c87edbbb81e6e338ac3f8733114c5c4e
clearsky77 <clearsky3264@gmail.com>
2021-10-02 14:45:19 +0900

    프래그먼트 페이징 담당하는 어댑터 작업중 - 상속/생성자 => 기초 작업 - 필수 구형 함수 2개 오버라이딩

---
+++ app/src/main/java/com/clearsky77/viewpager_review/adapters/ManinViewPagerAdapter.kt
@@ -0,0 +1,16 @@
+package com.clearsky77.viewpager_review.adapters
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+
+class ManinViewPagerAdapter(fm : FragmentManager) : FragmentPagerAdapter(fm) {
+    override fun getCount(): Int {
+
+    }
+
+    override fun getItem(position: Int): Fragment {
+
+    }
+
+}
